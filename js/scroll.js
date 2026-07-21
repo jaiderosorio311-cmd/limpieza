34c7d586d601e9e42c38370e3a522cbf8fe932ac
jaiderosorio311-cmd <jaiderosorio311@gmail.com>
2026-07-20 19:48:42 -0500

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,25 @@
+const elementos = document.querySelectorAll(
+".reveal,.reveal-left,.reveal-right,.reveal-scale"
+);
+
+function mostrarElementos(){
+
+    const alto = window.innerHeight;
+
+    elementos.forEach((elemento)=>{
+
+        const posicion = elemento.getBoundingClientRect().top;
+
+        if(posicion < alto - 100){
+
+            elemento.classList.add("active");
+
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll",mostrarElementos);
+
+mostrarElementos();
